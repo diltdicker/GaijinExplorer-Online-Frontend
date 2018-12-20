20cf562c5957baed76022403176e55733cd127f0
dillon <dillon@dickersonsoftware.com>
2018-12-20 00:52:16 -0700

    added navbar and ngx responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ResponsiveModule } from 'ngx-responsive';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ResponsiveModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
