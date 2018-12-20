3c92c12d72dd0975c0cdffebc409d956e1514678
diltdicker <diltdicker@gmail.com>
2018-12-20 14:54:35 -0700

    added 404 + routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { ResponsiveModule } from 'ngx-responsive';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { MangaSelectionComponent } from './manga-selection/manga-selection.component';
+import { MangaComponent } from './manga/manga.component';
+import { ChapterComponent } from './chapter/chapter.component';
+import { SearchResultComponent } from './search-result/search-result.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    MangaSelectionComponent,
+    MangaComponent,
+    ChapterComponent,
+    SearchResultComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/manga-selection/manga-selection.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manga-selection',
+  templateUrl: './manga-selection.component.html',
+  styleUrls: ['./manga-selection.component.scss']
+})
+export class MangaSelectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
